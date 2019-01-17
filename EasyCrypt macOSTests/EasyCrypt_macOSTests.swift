6d52a5bb3d64852da79ab5639d97abe5c68970c5
Łukasz Szarkowicz <lukasz@szarkowicz.info>
2019-01-17 16:34:39 +0100

    Init commit - created framework with all targets; added script for making auto documentation

---
+++ EasyCrypt macOSTests/EasyCrypt_macOSTests.swift
@@ -0,0 +1,33 @@
+//
+//  EasyCrypt_macOSTests.swift
+//  EasyCrypt macOSTests
+//
+//  Created by Lukasz Szarkowicz on 17.01.2019.
+//
+
+import XCTest
+@testable import EasyCrypt_macOS
+
+class EasyCrypt_macOSTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
---
+++ EasyCrypt/macOS/EasyCrypt.framework/Versions/A/Headers/EasyCrypt_macOS.h
@@ -0,0 +1,18 @@
+//
+//  EasyCrypt_macOS.h
+//  EasyCrypt macOS
+//
+//  Created by Lukasz Szarkowicz on 17.01.2019.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EasyCrypt_macOS.
+FOUNDATION_EXPORT double EasyCrypt_macOSVersionNumber;
+
+//! Project version string for EasyCrypt_macOS.
+FOUNDATION_EXPORT const unsigned char EasyCrypt_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyCrypt_macOS/PublicHeader.h>
+
+
